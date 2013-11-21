73a3cc3eba63efb61f21860e3ed011cf02065205
den <jsbot0001@gmail.com>
2013-11-21 22:34:22 +0200

    start HTML parser

--- clientSatanCMS/js/cage/appSP/views/page/pageView.js
+++ clientSatanCMS/js/cage/appSP/views/page/pageView.js
@@ -20,12 +20,14 @@ define([
 		events: {
 			"click .gtreg": "updateData"
 		},
+        newObj: {},
+
 		updateData: function(){
 			var defaults = {};
 			defaults.casinoName= $('#api_casinoName').val();
 			defaults.systemId= $('#api_systemId').val();
-
-			conn.req('updateApi', " id="+$('#api_id').val()
+            this.preverse($(".entry"));
+			/*conn.req('updateApi', " id="+$('#api_id').val()
 				,   "  protocol="+$('#api_protocol').val()+
 					"| path="+$('#api_path').val()+
 					"| fallbackPath="+$('#api_fallbackPath').val()+
@@ -33,21 +35,56 @@ define([
 					"| defaults = "+JSON.stringify(defaults)
 				, function (data) {
 				console.log(data);
-			});
+			});*/
 		},
 		render: function () {
 			var context = this.model;
 			var html = this.template({api: context.toJSON()});
 			this.$el.html(html);
 		},
+        preverse: function(elem){
+            var _this = this;
+            //$(elem).children().each(function(i,e){
 
+                $(elem).children().each(function(i,el){
+                    if($(el).children().length>0){
+
+                        _this.preverse($(el));
+
+                    }else{
+
+                        if ($(el).attr('d-bind')!= undefined){
+
+
+                        if ($(el).parent().attr("d-bind")!=undefined){
+                            console.log($(el).attr('d-bind'));
+                            var tobj = {};
+
+                            tobj[$(el).attr('d-bind')]=$(el).val();
+                            console.log(tobj);
+                            _this.newObj[$(el).parent().attr("d-bind")]=tobj;
+                        }else{
+                            _this.newObj[$(el).attr('d-bind')]=$(el).val();
+                        }
+
+                        }
+
+                    }
+                    //console.log($(el).attr('d-bind'));
+
+                });
+                console.log(_this.newObj);
+                //console.log("length:"+ $(e).children().length);
+
+            //});
+        },
 		initialize: function () {
 			var _this = this;
 			Handlebars.registerHelper('ifObject', function(item, target, options) {
 				if(typeof item == "object") {
 					var html = _this.subObjtemplate({obj:item, target:target});
 
-                    console.log(html);
+                    //console.log(html);
 
                     return new Handlebars.SafeString(html);
 				} else {
@@ -75,16 +112,11 @@ define([
 					defaults: {casinoName: modelData.defaults.casinoName, systemId: modelData.defaults.systemId},
 					testObj: {field:modelData.testObj.field ,innerObj: {data:modelData.testObj.innerObj.data,someField:modelData.testObj.innerObj.someField}}
 				}*/
+                //console.log(modelData);
 				_this.model.set(modelData);
 
 				_this.render();
-				_this.model.on('change:testObj.field',function(){
-					console.log("---------TEST FIELD-------------->");
-					console.log(
-					_this.model.get("testObj").field
-					);
-				});
-				_this.model.get("testObj").field = "bla";
+
 
 			});
 
